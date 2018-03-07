52e10bb23301b947e7dead347c2aac68984c2bd0
Yusan Yin <v-yuyin@microsoft.com>
2018-03-06 18:49:36 -0800

    Add Character Creation

---
+++ adventures_of_Yuane/adventures_of_Yuane/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace adventures_of_Yuane
+{
+    public class Player : Character
+    {
+        int points = 100;
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"{name} Hp  : {Hp}");
+            Console.WriteLine($"{name} Str : {Str}");
+            Console.WriteLine($"{name} Def : {Def}");
+            Console.WriteLine();
+        }
+
+        public void SpendPoints()
+        {
+            string choice = "default";
+            while (this.points > 0 || choice == "cancel")
+            {
+                DisplayStats();
+                Console.WriteLine($"You have {points} to spend on your stats!");
+                Console.WriteLine("How many points would you like to spend?");
+                var spend = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine($"Where would you like to spend your {spend} points?");
+                choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "hp":
+                        Hp += spend;
+                        points -= spend;
+                        break;
+                    case "str":
+                        Str += spend;
+                        points -= spend;
+                        break;
+                    case "def":
+                        Str += spend;
+                        points -= spend;
+                        break;
+                    case "cancel":
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+        public Player Create()
+        {
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            SpendPoints();
+            DisplayStats();
+            return this;
+        }
+
+    }
+}
--- adventures_of_Yuane/adventures_of_Yuane/Program.cs
+++ adventures_of_Yuane/adventures_of_Yuane/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,75 @@ namespace adventures_of_Yuane
     {
         static void Main(string[] args)
         {
-            var player = new Character();
+            var player = new Player().Create();
 
-            Console.WriteLine("What's your name?");
-            player.name = Console.ReadLine();
+            var enemy = new Character {name = "Goblin Bully"};
             
-
-            Console.WriteLine("Character Name :" + player.name);
-            Console.WriteLine("Character Health :" + player.hp);
-            Console.WriteLine("Character Strength :" + player.str);
-
+            Console.WriteLine($"A {enemy.name} appeared!");
             Console.ReadKey();
         }
     }
     public class Character
     {
         public string name;
-        public int hp;
-        public int str;
+        public int Hp = 100;
+        public int Str = 10;
+        public int Def = 10;
+    }
+
+    public class Player : Character
+    {
+        int points = 100;
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"{name} Hp  : {Hp}");
+            Console.WriteLine($"{name} Str : {Str}");
+            Console.WriteLine($"{name} Def : {Def}");
+            Console.WriteLine();
+        }
+
+        public void SpendPoints()
+        {
+            string choice = "default";
+            while (this.points > 0 || choice == "cancel")
+            {
+                DisplayStats();
+                Console.WriteLine($"You have {points} to spend on your stats!");
+                Console.WriteLine("How many points would you like to spend?");
+                var spend = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine($"Where would you like to spend your {spend} points?");
+                choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "hp":
+                        Hp += spend;
+                        points -= spend;
+                        break;
+                    case "str":
+                        Str += spend;
+                        points -= spend;
+                        break;
+                    case "def":
+                        Str += spend;
+                        points -= spend;
+                        break;
+                    case "cancel":
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+        public Player Create()
+        {
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            SpendPoints();
+            DisplayStats();
+            return this;
+        }
+
     }
 }
